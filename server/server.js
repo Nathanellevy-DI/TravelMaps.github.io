c0f27384cf6c8a1c06ce0d8983dc7f67697cce8b
Nathanel Levy <nathanellevy@gmail.com>
2026-07-15 13:47:21 +0300

    Force IPv4 DNS at very top of server.js entry point

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+// Force IPv4 DNS resolution (Render free tier doesn't support IPv6)
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
